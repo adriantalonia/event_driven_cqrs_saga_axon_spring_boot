f989cc9f8986152e71fbfddf874a5be1b924eee6
Adrian <adrian.talonia11@gmail.com>
2022-10-06 21:28:59 -0500

    add validation

--- product-service/src/main/java/com/eventproject/productservice/command/rest/ProductCommandController.java
+++ product-service/src/main/java/com/eventproject/productservice/command/rest/ProductCommandController.java
@@ -1,12 +1,12 @@
 package com.eventproject.productservice.command.rest;
 
 import com.eventproject.productservice.command.CreateProductCommand;
-import com.eventproject.productservice.core.model.dto.ProductRequest;
 import lombok.AllArgsConstructor;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -19,7 +19,7 @@ public class ProductCommandController {
 
 
     @PostMapping
-    public String createProduct(@RequestBody ProductRequest product) {
+    public String createProduct(@Valid @RequestBody CreateProductRestModel product) {
 
         CreateProductCommand createProductCommand = CreateProductCommand.builder()
                 .price(product.getPrice())
